01f5b08cbd88cf40912228681d72e08b345022a7
arthurgabriel2studiogames-coder <arthur.gabriel.2.studiogames@gmail.com>
2026-02-06 15:21:36 -0300

    Create atividade.py
    primeira versao do codigo

---
+++ atividade.py
@@ -0,0 +1,9 @@
+variavel=13
+minha_idade="minha idade "
+print(minha_idade + str(variavel)) 
+a_minha_altura=1.63
+variavel+=a_minha_altura
+#print("e minha altura " + str(a_minha_altura )+" somada a mimhna idade resulta em "+str (variavel))
+altura=True
+idade=False
+#print(str (altura)  +str (idade))
