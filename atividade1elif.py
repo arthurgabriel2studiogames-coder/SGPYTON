26ca91904a2db7aab68ec5c0ec5df6b44e1801c4
arthurgabriel2studiogames-coder <arthur.gabriel.2.studiogames@gmail.com>
2026-03-27 15:34:00 -0300

    Create atividade1elif.py

---
+++ atividade1elif.py
@@ -0,0 +1,12 @@
+nota1 = int(input("Digite sua nota: "))
+nota2= int(input("Digite sua nota: "))
+nota3 = int(input("Digite sua nota: "))
+media= (nota1 +  nota2 + nota3 )/ 3
+if media==10:
+    print ("nota perfeita")
+
+elif media>4:
+    print("aprovado")
+
+else : 
+    print ("reprovado")
