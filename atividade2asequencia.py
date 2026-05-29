34b557637620f875fa1b5d5d353b9b5c8cd0c314
arthurgabriel2studiogames-coder <arthur.gabriel.2.studiogames@gmail.com>
2026-05-29 15:49:07 -0300

    Create atividade2asequencia.py

---
+++ atividade2asequencia.py
@@ -0,0 +1,8 @@
+listatutu = [ "the crew", "fifa" ,"supermen 64","forsaken" ]
+for jogo in listatutu:
+    if jogo=="the crew":
+        print(jogo +  " gosto desse jogo")
+    elif jogo=="forsaken":
+        print(jogo+ " pior jogo de todas ")
+    else:
+        print(jogo+ " esse jogo e ok")
