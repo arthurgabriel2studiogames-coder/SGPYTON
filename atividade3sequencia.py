e477ef414d3c802cf7ce372e9e91f80045afc4cb
arthurgabriel2studiogames-coder <arthur.gabriel.2.studiogames@gmail.com>
2026-06-05 16:02:04 -0300

    Create atividade3sequencia.py

---
+++ atividade3sequencia.py
@@ -0,0 +1,14 @@
+palavra = input("digite uma palavra: ")
+for letra in palavra:
+    if letra=="a":
+        print("a e uma letra  de arthur")
+    elif letra== "r":
+        print("r e uma letra de arthur")
+    elif letra== "t":    
+        print("t e uma letra de arthur")
+    elif letra== "h":     
+        print("h e uma letra de arthur")
+    elif letra== "u":     
+        print("u e uma letra de arthur")
+    else:
+        print(letra+" nao e uma letra de arthur ")    
